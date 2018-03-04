4f2851a9946b3790714fb75ea10a8f980fceaefd
AleksandrPeshcherov <kawalot@gmail.com>
2018-03-04 21:58:38 +0200

    Handling Migration Failures

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length=15, unique=True)
+    timestamp = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.url)
